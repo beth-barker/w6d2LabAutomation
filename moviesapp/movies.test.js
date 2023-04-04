eb3fd09dbfb08627949358285e705cc7632fd8fb
Beth Barker <barker.k.elizabeth@gmail.com>
2023-04-04 16:10:27 -0600

    finished automation lab

---
+++ moviesapp/movies.test.js
@@ -0,0 +1,43 @@
+const {Builder, Browser, By, Key, until} = require("selenium-webdriver");
+
+let driver;
+
+beforeEach(async () => {
+    driver = new Builder().forBrowser(Browser.CHROME).build();
+});
+
+afterEach(async () => {
+    await driver.quit();
+});
+
+
+test("test that add movie function works", async () => {
+    await driver.get('http://localhost:3001/');
+
+    await driver.findElement(By.name("movieTitle")).sendKeys("Star Wars", Key.RETURN);
+    await driver.sleep(2000);
+    
+    // await driver.findElement(By.name('btnAdd')).click();
+
+    await driver.sleep(2000);
+});
+
+test("test that removes a movie from the list", async () => {
+    await driver.get('http://localhost:3001/');
+
+    await driver.findElement(By.name("movieTitle")).sendKeys("Cinderella", Key.RETURN);
+    await driver.sleep(2000);
+
+    await driver.findElement(By.className("delete-btn")).click();
+    await driver.sleep(2000);
+})
+
+test("test that a movie has been watched with selected checkbox", async () => {
+    await driver.get('http://localhost:3001/');
+
+    await driver.findElement(By.name("movieTitle")).sendKeys("Ratatoullie", Key.RETURN);
+    await driver.sleep(2000);
+
+    await driver.findElement(By.id("movie-0")).click();
+    await driver.sleep(2000);
+});
